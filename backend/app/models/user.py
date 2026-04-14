e7819ae38d58dcdc6de58ae45531d2e88fff18bc
masa276 <vividlancer@gmail.com>
2026-04-14 12:00:49 +0900

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,16 @@
 from sqlalchemy import Column, Integer, String
-from app.db.session import Base
+from sqlalchemy.orm import relationship
+from app.db.base import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+    tasks = relationship(
+        "Task",
+        back_populates="user",
+        cascade="all, delete"
+    )
