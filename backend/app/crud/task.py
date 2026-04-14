c816901cfece32d4e21e1f9e8592898598b957f0
masa276 <vividlancer@gmail.com>
2026-04-14 13:13:36 +0900

    Update task.py

--- backend/app/crud/task.py
+++ backend/app/crud/task.py
@@ -1,13 +1,17 @@
 from app.models.task import Task
 
+# 作成
 def create_task(db, task, user_id: int):
     db_task = Task(
         title=task.title,
         user_id=user_id
     )
-
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
-
     return db_task
+
+
+# 取得（これが重要）
+def get_tasks(db, user_id: int):
+    return db.query(Task).filter(Task.user_id == user_id).all()
