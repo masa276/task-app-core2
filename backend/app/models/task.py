9dccf1047be758dd48b38c72140bc7ed41f0cee5
masa276 <vividlancer@gmail.com>
2026-04-14 11:16:48 +0900

    Update task.py

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.session import Base
 
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", backref="tasks")
