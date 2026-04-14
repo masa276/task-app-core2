f3c7e5edcc42f193e877a054d2c5107b8faf18b2
masa276 <vividlancer@gmail.com>
2026-04-14 13:37:59 +0900

    Update task.py

--- backend/app/crud/task.py
+++ backend/app/crud/task.py
@@ -4,7 +4,7 @@ from app.models.task import Task
 def create_task(db, task, user_id: int):
     db_task = Task(
         title=task.title,
-        user_id=user_id
+        #user_id=user_id
     )
     db.add(db_task)
     db.commit()
