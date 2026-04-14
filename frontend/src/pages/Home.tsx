a2e6a900f18c15498ad81ba1410d01270fefd1b7
masa276 <vividlancer@gmail.com>
2026-04-14 11:18:23 +0900

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,1 +1,22 @@
+import { useEffect, useState } from "react";
+import { getTasks, createTask } from "../api/task";
 
+export default function Home() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks().then(setTasks);
+  }, []);
+
+  return (
+    <div>
+      <h1>Task List</h1>
+
+      <ul>
+        {tasks.map((t: any) => (
+          <li key={t.id}>{t.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
